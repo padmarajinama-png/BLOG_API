3ec66b2354d9fc8c686f4dbfdff9dfe9d35a367e
padmarajinama-png <padmarajinama@gmail.com>
2026-07-07 16:21:41 +0530

    Fix ImageRepository

--- BLOG.API/Repositories/Implementation/ImageRepository.cs
+++ BLOG.API/Repositories/Implementation/ImageRepository.cs
@@ -1,28 +1,30 @@
-﻿﻿using Blog.API.Data;
-using Blog.API.Models.Domain;
-using Blog.API.Models.DTO;
-using Blog.API.Reposittory.Interface;
-using BLOG.API.Data;
+﻿using BLOG.API.Data;
+using BLOG.API.Models.Domain;
 using BLOG.API.Repositories.Interface;
+using Microsoft.EntityFrameworkCore;
 using CloudinaryDotNet;
 using CloudinaryDotNet.Actions;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using BLOG.API.Models.DTO;
 
 
 namespace BLOG.API.Repositories.Implementation
 {
     public class ImageRepository : IImageRepository
+    
     {
-        private readonly Cloudinary cloudinary;
+         private readonly Cloudinary cloudinary;
+       
         private readonly ApplicationDbContext dbContext;
 
-        public ImageRepository(IOptions<CloudinarySettings> cloudinaryOptions, ApplicationDbContext dbContext)
+         public ImageRepository(IOptions<CloudinarySettings> cloudinaryOptions, ApplicationDbContext dbContext)
         {
             var settings = cloudinaryOptions.Value;
             this.cloudinary = new Cloudinary(new Account(settings.CloudName, settings.ApiKey, settings.ApiSecret));
             this.dbContext = dbContext;
         }
+
+
         public async Task<IEnumerable<BlogImage>> GetAll()
 
         {
@@ -40,14 +42,14 @@ namespace BLOG.API.Repositories.Implementation
                 Overwrite = false
             };
 
-            var uploadResult = await cloudinary.UploadAsync(uploadParams);
+             var uploadResult = await cloudinary.UploadAsync(uploadParams);
 
             if (uploadResult.Error != null)
             {
                 throw new InvalidOperationException($"Cloudinary upload failed: {uploadResult.Error.Message}");
             }
-
-            blogImage.Url = uploadResult.SecureUrl.AbsoluteUri;
+            
+           blogImage.Url = uploadResult.SecureUrl.AbsoluteUri;
 
             await dbContext.BlogImages.AddAsync(blogImage);
             await dbContext.SaveChangesAsync();
